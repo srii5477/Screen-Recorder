a117cb615c4b7a2c22dc907f05609fadff0fda1c
Sride Shankar <sridevishankar1161@gmail.com>
2024-07-25 21:04:05 +0530

    add option to quit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pyautogui
 import numpy as np
 import cv2
+import keyboard
 
 # create a video writer object
 resolution = (1920, 1080)
@@ -18,24 +19,26 @@ obj = cv2.VideoWriter(name, code, fps, resolution)
 
 
 while True:
- 
     # take a ss
-    
     ss = pyautogui.screenshot()
  
     # convert the screenshot to a numpy array
-    
     frame = np.array(ss)
  
+    # default opencv capture is in bgr color scheme (inverted)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     
     # write it to the output file
     
     obj.write(frame)
     
-    
-# discard waste
-    
+    if keyboard.is_pressed('q'):
+        break 
+
+# discard video writer object and close windows   
+cv2.destroyAllWindows
+
 obj.release()
- 
-cv2.destroyAllWindows()
+    
+    
+
