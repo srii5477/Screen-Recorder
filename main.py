4bd276f36e811cd73d3615d5f68459930b671afd
Sride Shankar <sridevishankar1161@gmail.com>
2024-07-27 12:00:03 +0530

    transition to a cli tool

--- main.py
+++ main.py
@@ -2,43 +2,59 @@ import pyautogui
 import numpy as np
 import cv2
 import keyboard
+import typer
+from rich import print as rprint
+import os
 
-# create a video writer object
-resolution = (1920, 1080)
+app = typer.Typer()
 
-# specify how to compress & encode the video
-code = cv2.VideoWriter_fourcc(*"XVID")
+@app.command("Start")
+def func():
+    rprint("[blink cyan bold] Welcome to ScreenRec 1.0 [/blink cyan bold] [red bold] Press y to get started or q to quit [/red bold]")
+    cmd = input("Enter y to get started: ")
+    if cmd == 'q':
+        rprint("[magenta bold] Thank you for trying out ScreenRec! [/magenta bold]")
+    elif cmd == 'y':
+        # create a video writer object
+        resolution = (1920, 1080)
 
-# name the output 
-name = "ScreenRec.avi"
+        # specify how to compress & encode the video
+        code = cv2.VideoWriter_fourcc(*"XVID")
 
-# specify frame rate, will experiment with it to obtain the best result
-fps = 60
+        # name the output 
+        name = "ScreenRec.avi"
 
-obj = cv2.VideoWriter(name, code, fps, resolution)
+        # specify frame rate, will experiment with it to obtain the best result
+        fps = 60
+
+        obj = cv2.VideoWriter(name, code, fps, resolution)
 
 
-while True:
-    # take a ss
-    ss = pyautogui.screenshot()
- 
-    # convert the screenshot to a numpy array
-    frame = np.array(ss)
- 
-    # default opencv capture is in bgr color scheme (inverted)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    
-    # write it to the output file
-    
-    obj.write(frame)
-    
-    if keyboard.is_pressed('q'):
-        break 
+        while True:
+            # take a ss
+            ss = pyautogui.screenshot()
+        
+            # convert the screenshot to a numpy array
+            frame = np.array(ss)
+        
+            # default opencv capture is in bgr color scheme (inverted)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            
+            # write it to the output file
+            
+            obj.write(frame)
+            
+            if keyboard.is_pressed('q'):
+                rprint("[magenta bold] Your recording has been saved. Thanks for using ScreenRec! [/magenta bold]")
+                break 
 
-# discard video writer object and close windows   
-cv2.destroyAllWindows
+        # discard video writer object and close windows   
+        cv2.destroyAllWindows
 
-obj.release()
+        obj.release()
+        
+app()
+
     
     
 
