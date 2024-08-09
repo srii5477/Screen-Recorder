43dcf2c731d0a2b83071691a391b5f5f967f8755
Sride Shankar <sridevishankar1161@gmail.com>
2024-08-09 20:14:21 +0530

    added a gui interface

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Don't pay for an essential functionality, screen record to your heart's content with ScreenRec - a CLI tool with a pretty cool interface and you need only 2 options to navigate the tool - a 'q' to quit and a 'y' to start.
 
+ScreenRec now has a GUI.
+
 ### How do I use it? ###
 
 open up your terminal
--- main.py
+++ main.py
@@ -2,59 +2,55 @@ import pyautogui
 import numpy as np
 import cv2
 import keyboard
-import typer
-from rich import print as rprint
-from rich.panel import Panel
-import os
-
-app = typer.Typer()
-
-@app.command("Start")
-def func():
-    rprint(Panel("[blink cyan bold] Welcome to ScreenRec 1.0 [/blink cyan bold] [red bold] Press y to get started or q to quit [/red bold]"))
-    cmd = input("Enter an input: ")
-    if cmd == 'q':
-        rprint("[magenta bold] Thank you for trying out ScreenRec! [/magenta bold]")
-    elif cmd == 'y':
-        # create a video writer object
-        resolution = (1920, 1080)
-
-        # specify how to compress & encode the video
-        code = cv2.VideoWriter_fourcc(*"XVID")
-
-        # name the output 
-        name = "ScreenRec.avi"
-
-        # specify frame rate, will experiment with it to obtain the best result
-        fps = 60
-
-        obj = cv2.VideoWriter(name, code, fps, resolution)
+import tkinter as tk
 
 
-        while True:
-            # take a ss
-            ss = pyautogui.screenshot()
+def handle_start(event):
+     # create a video writer object
+    resolution = (1920, 1080)
+
+    # specify how to compress & encode the video
+    code = cv2.VideoWriter_fourcc(*"XVID")
+
+    # name the output 
+    name = "ScreenRec.avi"
+
+    # specify frame rate, will experiment with it to obtain the best result
+    fps = 60
+
+    obj = cv2.VideoWriter(name, code, fps, resolution)
+
+
+    while True:
+        # take a ss
+        ss = pyautogui.screenshot()
         
-            # convert the screenshot to a numpy array
-            frame = np.array(ss)
+        # convert the screenshot to a numpy array
+        frame = np.array(ss)
         
-            # default opencv capture is in bgr color scheme (inverted)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+         # default opencv capture is in bgr color scheme (inverted)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             
-            # write it to the output file
+        # write it to the output file
             
-            obj.write(frame)
+        obj.write(frame)
             
-            if keyboard.is_pressed('q'):
-                rprint("[magenta bold]Your recording has been saved. Thanks for using ScreenRec! [/magenta bold]")
-                break 
+        if keyboard.is_pressed('q'):
+            break 
 
-        # discard video writer object and close windows   
-        cv2.destroyAllWindows
+    # discard video writer object and close windows   
+    cv2.destroyAllWindows
 
-        obj.release()
-        
-app()
+    obj.release()
+
+window = tk.Tk()
+greeting = tk.Label(text="Welcome to ScreenRec. \n Once you start recording, press the close button to stop. Would you like to-", width=60, height=20, bg="magenta")
+greeting.pack(fill=tk.X)
+btn = tk.Button(text="Start", bg="green", fg="white")
+btn.bind("<Button-1>", handle_start)
+btn.pack(fill=tk.X)
+window.mainloop()
+print("done")
 
     
     
